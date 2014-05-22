a624c20e8b20ffce09566c6a02a349b4ec8611c0
Rajeev <rajeev@Rajeevs-MacBook-Pro.local>
2014-05-22 14:23:14 +0530

    Fixed bug in GetTopNTargetRankers.

--- V_1.2.1/App42_Cocos2dX_3.0Beta2_SDK/GameService/ScoreBoardService.cpp
+++ V_1.2.1/App42_Cocos2dX_3.0Beta2_SDK/GameService/ScoreBoardService.cpp
@@ -844,7 +844,7 @@ void ScoreBoardService::GetTopNTargetRankers(string gameName,int max,cocos2d::Ob
      * Creating URL
      */
     string resource = "game/scoreboard/";
-    resource.append(gameName+"/");
+    resource.append(gameName);
     resource.append("/targetedrankers/");
     resource.append(Util::ItoA(max));
 	string url = getBaseUrl(resource);
