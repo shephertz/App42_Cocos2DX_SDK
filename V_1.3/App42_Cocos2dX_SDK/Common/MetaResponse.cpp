52262de2938ee7b7e8fef7b9c307c0ec7e0ed060
RajeevRShephertz <rajeev.ranjan@shephertz.co.in>
2015-01-06 16:10:28 +0530

    Moving to 1.x

--- V_1.3/App42_Cocos2dX_SDK/Common/MetaResponse.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  MetaResponse.cpp
-//  App42CPPSDK
-//
-//  Created by Rajeev Ranjan on 25/02/14.
-//
-//
-
-#include "MetaResponse.h"
--- V_1.4/App42_Cocos2dX_SDK/SocialService/App42Friend.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  App42Friend.cpp
-//  App42Cocos2dX3.0Sample
-//
-//  Created by Shephertz Technologies Pvt Ltd on 26/06/14.
-//
-//
-
-#include "App42Friend.h"
